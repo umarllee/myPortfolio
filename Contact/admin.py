35a228eefc162532ec1d3c632ab5bf5315ee82ee
umarllee <gulchohreum20631@sabah.edu.az>
2021-06-30 07:35:25 +0400

    10th commit

--- Contact/admin.py
+++ Contact/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from . models import Contact
 
-admin.site.register(Contact)
+
+class contactadmin(admin.ModelAdmin):
+    fields= ["name" , "lastname" , "email" , "message"]
+
+admin.site.register(Contact,contactadmin)
